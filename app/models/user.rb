d067aeb7ece5c4bc3145cd277d04d497b5093368
EC2 Default User <ec2-user@ip-172-31-46-239.ap-northeast-1.compute.internal>
2023-08-17 10:19:28 +0000

    [add]enumの導入、定義、日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  #性別選択用
+  enum gender: { man: 0, woman: 1 }
 end
