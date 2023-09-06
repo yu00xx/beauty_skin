120f77391229e43588c61908857bb20c4a764ced
EC2 Default User <ec2-user@ip-172-31-46-239.ap-northeast-1.compute.internal>
2023-09-06 10:07:25 +0000

    [update]退会済みのユーザーをログインさせない機能を追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
  # before_action :configure_sign_in_params, only: [:create]
+ before_action :user_state, only: [:create]
 
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
@@ -26,7 +27,19 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  #退会済みのユーザーか確認する
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    if @user.valid_password?(params[:user][:password])
+      if @user.is_deleted?
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+        redirect_to new_user_registration_path
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
