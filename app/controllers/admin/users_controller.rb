9cdda0a5b89d0ca16850fbdbaad198c5a1050fab
EC2 Default User <ec2-user@ip-172-31-46-239.ap-northeast-1.compute.internal>
2023-09-16 12:35:50 +0000

    [add]管理者側の会員一覧、会員詳細ページを作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,19 @@
 class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.page(params[:page]).per(10)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.page(params[:page])
+    @like_posts = @user.like_posts
+  end
+
+  def withdraw
+    @user = User.find(params[:id])
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to admin_users_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :users, only: [:index, :show]
+    patch 'users/withdraw', to: "users#withdraw"
     resources :posts, only: [:index, :show,]
     resources :categories, only: [:index, :create, :edit]
   end
