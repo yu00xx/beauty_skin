ac4e4797ac04beea46a705c9fbe729f03a608eaf
EC2 Default User <ec2-user@ip-172-31-46-239.ap-northeast-1.compute.internal>
2023-09-25 16:08:17 +0000

    [update](管理者)投稿一覧画面のレイアウトを整える

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,7 @@
 class Admin::PostsController < ApplicationController
 
   def index
-    @posts = Post.page(params[:page])
+    @posts = Post.page(params[:page]).per(10)
     @categories = Category.all
   end
 
